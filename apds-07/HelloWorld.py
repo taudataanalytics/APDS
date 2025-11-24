c08582aaaf72832feaf04e9f6ccad378ca825596
taudata <taudata.analytics@gmail.com>
2025-11-24 11:53:18 +0700

    7

---
+++ apds-07/HelloWorld.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 24 09:32:51 2025
+
+@author: Taufik Sutanto
+"""
+
+print("Hello World from Cython.")
---
+++ apds-07/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 24 09:34:33 2025
+@author: Taufik Sutanto
+
+Compile from the terminal using the following command:
+    python setup.py build_ext --inplace
+"""
+
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize("HelloWorld.py")
+)

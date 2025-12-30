64b2ea2e966e1807d9cb9f9612839165b87a13d0
taudata Analytics <taudata.analytics@gmail.com>
2025-12-30 10:33:25 +0700

    update

--- apds-10.py
+++ apds-10.py
@@ -1,13 +1,12 @@
-from multiprocessing import Pool
-import math
+import multiprocessing as mp
 
-def cpu_task(n):
-    return sum(math.sqrt(i) for i in range(n))
+def f(x):
+    return x*x
 
-if __name__ == "__main__":
-    data = [10**7] * 4
-
-    with Pool(4) as pool:
-        results = pool.map(cpu_task, data)
-
-    print(results)
+if __name__ == '__main__':
+    print('Number of currently available processor = ', mp.cpu_count())
+    N = mp.cpu_count()
+    input_ = [1, 2, 3, 4, 5, 7, 9, 10]
+    print('input = ', input_)
+    with mp.Pool(N) as p:
+        print(p.map(f, input_))
